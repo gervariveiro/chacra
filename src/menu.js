6d898724e0ba0817c9229e62d9d2bc738152f3ce
unknown <gervasioriveiro@gmail.com>
2023-03-15 11:39:08 -0300

    1st comm

---
+++ src/menu.js
@@ -0,0 +1,14 @@
+const btnMenu = document.querySelector("#btnMenu");
+const menu= document.querySelector("#menu");
+btnMenu.addEventListener("click", function(){
+    menu.classList.toggle("mostrar");
+});
+
+
+/*function mostrar(){
+            document.getElementById('img').style.display = 'block';
+        }
+
+function ocultar(){
+            document.getElementById('img').style.display = 'none';
+        }*/
